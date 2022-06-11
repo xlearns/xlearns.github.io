3e1741561e405d88d7b2f525f22b7caff391569f
xlearns <2447684095@qq.com>
2022-06-11 21:43:49 +0800

    refactor(admin): update admin

---
+++ admin/assets/useMainElement.8ff50dfa.js
@@ -0,0 +1,1 @@
+import{j as t,R as n}from"./index.fec4e3df.js";const e=t(null),{width:s,height:i}=n(e);function r(){return{el:e,width:s,height:i}}export{r as u};
--- admin/assets/useMainElement.e1ef9a80.js
+++
@@ -1,1 +0,0 @@
-import{j as t,R as n}from"./index.a6f2b315.js";const e=t(null),{width:s,height:i}=n(e);function r(){return{el:e,width:s,height:i}}export{r as u};
