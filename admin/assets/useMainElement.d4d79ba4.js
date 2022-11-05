d6edbedc6aff43162f4e3378912ee8ba25e70472
xlearns <2447684095@qq.com>
2022-11-05 22:45:10 +0800

    chore: update admin

--- admin/assets/useMainElement.d4d79ba4.js
+++
@@ -1,1 +0,0 @@
-import{j as t,R as n}from"./index.af0c6673.js";const e=t(null),{width:s,height:i}=n(e);function r(){return{el:e,width:s,height:i}}export{r as u};
---
+++ admin/assets/useMainElement.f91118aa.js
@@ -0,0 +1,1 @@
+import{j as t,S as n}from"./index.2579fc99.js";const e=t(null),{width:s,height:i}=n(e);function r(){return{el:e,width:s,height:i}}export{r as u};
