5b0ac02a05c72948acab80f67008b4706af2b343
xlearns <2447684095@qq.com>
2022-11-14 10:14:14 +0800

    chore: update admin

--- admin/assets/useMainElement.999bc003.js
+++
@@ -1,1 +0,0 @@
-import{i as t,R as n}from"./index.2d28a344.js";const e=t(null),{width:i,height:s}=n(e);function r(){return{el:e,width:i,height:s}}export{r as u};
---
+++ admin/assets/useMainElement.e3136cb5.js
@@ -0,0 +1,1 @@
+import{i as t,R as n}from"./index.5a75210b.js";const e=t(null),{width:i,height:s}=n(e);function r(){return{el:e,width:i,height:s}}export{r as u};
